a24eb10fbff0ff923ed9d79bf7808a4d0d43115b
Siuliander <31857973+Siuliander@users.noreply.github.com>
2024-07-12 18:18:28 +0800

    Update index.js

--- clientes/index.js
+++ clientes/index.js
@@ -10,6 +10,8 @@ app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 app.use(express.static( path.join(__dirname, 'public')))
 
+console.log( "Diretorio: " + __dirname)
+
 /**
  * IMPORTANDO AS ROTAS
  */
