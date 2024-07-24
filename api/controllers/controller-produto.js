5c1b9177ea8d4857f45a6b1a0dcd554dcdb738a1
Siuliander <lendes2000@gmail.com>
2024-07-24 14:59:52 +0100

    2024-07-24 - Buscar All Produto - Controller

--- api/controllers/controller-produto.js
+++ api/controllers/controller-produto.js
@@ -1,4 +1,6 @@
-const modelCategoria = require("../models/model-categoria")
+const modelProduto = require("../models/model-produto")
+const modelPreco = require("../models/model-preco")
+const modelPrecoProduto = require("../models/model-preco-produto")
 
 const response = (data = [], error = []) => {
     if ( error ) { 
@@ -11,60 +13,67 @@ const response = (data = [], error = []) => {
 		data: data.rows.map( item => {
 			return {
 				id: item.id,
-				nome: item.categoria,
-				estado: item.estado,
-				id_estado: item.id_estado,
+				produto: item.produto,
+				descricao: item.descricao,
+				preco: item.preco,
+				img: item.img,
+				categoria: item.categoria,
 				request: {
 					method: 'GET',
 					descricao: 'Retorna Detalhe Específico',
-					url: `${process.env.API_HOST}:${process.env.API_PORT}/categoria/${item.id}`
+					url: `${process.env.API_HOST}:${process.env.API_PORT}/produto/${item.id}`
 				}
 			}
-		})
+		}),
+        message: data.message,
 	}
 	
 	return json
 }
 
 
-exports.getCategoriaAll = async (req) => {
+exports.getProdutoAll = async (req) => {
     let result = [];
     let error = null
     try {
-        const id = req.params.id || req.query.id || req.body.id ||  null;
+        const all = req.params.q || req.query.q || req.body.q ||  null;
+        const produto = req.params.produto || req.query.produto || req.body.produto ||  null;
         const categoria = req.params.categoria || req.query.categoria || req.body.categoria ||  null;
-        const estado = req.params.estado || req.query.estado || req.body.estado ||  2;
+        const preco = req.params.preco || req.query.preco || req.body.preco ||  null;
+
         const limit = req.params.limit || req.query.limit || req.body.limit || null;
 
-        result = await modelCategoria.select(id,categoria,estado,limit)
+        result = await modelProduto.select(null, all,produto,preco,categoria,limit)
         
     } catch (err) {
         // error = err
         console.log(err)
     } finally {
-        return response(modelCategoria.row(0,result),error)
+        return response(modelProduto.row(0,result),error)
     }
 }
 
-exports.getCategoriaID = async (req) => {
+exports.getProdutoID = async (req) => {
     let result = [];
     let error = null
     try {
         const id = req.params.id ||  null;
-        result = await modelCategoria.selectID(id)
+        result = await modelProduto.selectID(id)
     } catch (err) {
         console.log(err)
     } finally {
-        return response(modelCategoria.row(0,result),error)
+        return response(modelProduto.row(0,result),error)
     }
 }
 
-exports.postCategoria = async (req) => {
+exports.postProduto = async (req) => {
     let result = [];
     let error = null
     try {
+        const produto = req.params.produto || req.query.produto || req.body.produto ||  null;
         const categoria = req.params.categoria || req.query.categoria || req.body.categoria ||  null;
-        result = await modelCategoria.insert(categoria)
+        const preco = req.params.preco || req.query.preco || req.body.preco ||  null;
+        result = await modelProduto.insert(produto,preco,categoria)
         
     } catch (err) {
         // error = err
@@ -74,13 +83,13 @@ exports.postCategoria = async (req) => {
     }
 }
 
-exports.putCategoria = async (req) => {
+exports.putProduto = async (req) => {
     let result = [];
     let error = null
     try {
         const id = req.params.id || req.query.id || req.body.id ||  null;
         const categoria = req.params.categoria || req.query.categoria || req.body.categoria ||  null;
-        result = await modelCategoria.update(id,categoria,null,null)
+        result = await modelProduto.update(id,categoria,null,null)
         
     } catch (err) {
         // error = err
@@ -90,12 +99,12 @@ exports.putCategoria = async (req) => {
     }
 }
 
-exports.deleteCategoria = async (req) => {
+exports.deleteProduto = async (req) => {
     let result = [];
     let error = null
     try {
         const id = req.params.id || req.query.id || req.body.id ||  null;
-        result = await modelCategoria.deleted(id)
+        result = await modelProduto.deleted(id)
         
     } catch (err) {
         // error = err
